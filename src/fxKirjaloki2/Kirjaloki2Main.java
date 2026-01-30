5a92cdcbd7ec4e6b1363e4edc4d5490441094a57
heespari <heta.e.parikka@student.jyu.fi>
2026-01-30 23:37:31 +0200

    vaihe 2

---
+++ src/fxKirjaloki2/Kirjaloki2Main.java
@@ -0,0 +1,38 @@
+package fxKirjaloki2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+
+/**
+ * @author heta
+ * @version 30.1.2026
+ *
+ */
+public class Kirjaloki2Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader ldr = new FXMLLoader(getClass().getResource("Kirjaloki2GUIView.fxml"));
+            final Pane root = ldr.load();
+            //final Kirjaloki2GUIController kirjaloki2Ctrl = (Kirjaloki2GUIController) ldr.getController();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("kirjaloki2.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Kirjaloki");
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args Ei kaytossa
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
