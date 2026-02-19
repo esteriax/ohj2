c2d34e7cd2ecc2890ce307a47ea4d9bd34dec2e8
heespari <heta.e.parikka@student.jyu.fi>
2026-02-19 10:15:55 +0200

    Kirjailija-luokka muutettu vastaamaan Jäsen-luokkaa Harrastus-luokan sijaan

---
+++ src/kirjaloki/Kirjailija.java
@@ -0,0 +1,114 @@
+package kirjaloki;
+import java.io.*;
+
+
+/**
+ * Kirjalokin Kirjailija-luokka
+ * @author heta
+ */
+public class Kirjailija {
+    
+    private String nimi;
+    private int kirjailijaId;
+    private int syntymaVuosi;
+    private boolean suosikki;
+
+    private static int seuraavaNro = 1;
+    
+    
+    /**
+     * @return kirjailijan nimi
+     * @example
+     * <pre name="test">
+     *   Kirjailija kytomaki = new Kirjailija();
+     *   kytomati.vastaaKytomaki();
+     *   ky.getNimi() === "Anni Kytömäki";
+     * </pre>
+     */
+    public String getNimi() {
+        return nimi;
+
+    }
+    
+    /**
+     * Apumetodi, jolla saadaan täytettyä testiarvot kirjailijalle.
+     */
+    public void vastaaKytomaki() {
+            nimi= "Anni Kytömäki";
+            syntymaVuosi = 1980;
+            suosikki = true;
+        }
+    /**
+     * Tulostetaan kirjailijan tiedot
+     * @param out tietovirta johon tulostetaan
+     */
+    public void tulosta(PrintStream out) {
+        out.println(String.format("%03d", kirjailijaId) + " " + nimi);
+        out.println("Syntymävuosi: " + syntymaVuosi);
+        out.println("Onko suosikki: " + suosikki);
+        out.println();
+    }
+    
+    /**
+     * Tulostetaan kirjailijan tiedot
+     * @param os tietovirta johon tulostetaan
+     */
+    public void tulosta(OutputStream os) {
+        tulosta(new PrintStream(os));
+
+    }
+
+    
+    /**
+     * Palauttaa kirjailijan tunnusnumeron.
+     * @return kirjailijan tunnusnumeron
+     * @example
+     * <pre name="test">
+     *   Kirjailija kirjailija1 = new Kirjailija();
+     *   kirjailia1.getTunnusNro() === 0;
+     *   kirjailia1.rekisteroi();
+     *   Kirjailija kirjailija2 = new Kirjailija();
+     *   kirjailija2.rekisteroi();
+     *   int n1 = kirjailija1.getTunnusNro();
+     *   int n2 = kirjailija2.getTunnusNro();
+     *   n1 === n2-1;
+     * </pre>
+     */
+     public int rekisteroi() {
+         kirjailijaId = seuraavaNro;
+         seuraavaNro++;
+         return kirjailijaId;
+
+        }
+
+
+     /**
+      * Palauttaa kirjailijan tunnusnumeron.
+      * @return kirjailijan tunnusnumero
+      */
+     public int getKirjailijaId() {
+         return kirjailijaId;
+        }
+
+    
+    /**
+     * Testataan Kirjailija-luokkaa
+     * @param args ei käytössä
+     */
+    public static void main(String[] args) {
+        
+        Kirjailija kytomaki1 = new Kirjailija();
+        Kirjailija kytomaki2 = new Kirjailija();
+
+        kytomaki1.rekisteroi();
+        kytomaki2.rekisteroi();
+        kytomaki1.vastaaKytomaki();
+        kytomaki2.vastaaKytomaki();
+        kytomaki1.tulosta(System.out);
+        kytomaki2.tulosta(System.out);
+         
+
+    }
+
+}
+
